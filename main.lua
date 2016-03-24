a1a2be9f7acc8e6b068d84cc400ec9113d052195
Christian Afonso <christian.afonso@wohlhart.at>
2016-03-24 12:09:38 +0100

    Added testMath function

--- main.lua
+++ main.lua
@@ -2,6 +2,19 @@ require "Network"
 
 -- Tests
 
+function testMath()
+  local arr1 = {1,2,3}
+  local arr2 = {4,5,6}
+  local arr3 = map(function(a) return a*a end, arr1)
+  assert(v_to_string(arr3) == "(1,4,9)")
+  local arr4 = mat_mult_vv(arr2, arr3)
+  assert(v_to_string(arr4) == "(4,20,54)")
+  local sum4 = sum(arr4)
+  assert(sum4 == 78)
+  local tostring = reduce(function(a,b) return a .. " " .. b end, arr4)
+  assert(tostring == "4 20 54")
+end
+
 function testNetworkSetup(net, sizes)
   print ("Network layers: " .. #net.sizes)
   assert(#net.sizes == #sizes, "Net layer count wrong (should be #sizes, is: " .. #net.sizes .. ")")
@@ -23,6 +36,7 @@ end
 sizes = {2,4,3}
 net = Network.new(sizes)
 print "=== Starting Network tests ==="
+testMath()
 testNetworkSetup(net, sizes)
 testCompute(net, {1,1})
 testCompute(net, {2,2})
