e1693bfce0d3c292a2fc7257f727b6d4c1c3094f
Christian Afonso <christian.afonso@wohlhart.at>
2016-04-11 14:38:46 +0200

    Added plot Löve2D folder to plot error graph

---
+++ plot/main.lua
@@ -0,0 +1,17 @@
+function love.draw()
+  love.graphics.clear()
+
+  local file = io.open("../errsums.csv", "r")
+  io.input(file)
+  local x = 0
+  local yoff = 200
+  local lasty = yoff
+  for line in io.lines() do
+    y = yoff - line * 10
+    love.graphics.line(x,lasty,x+1,y)
+
+    x = x + 1
+    lasty = y
+  end
+  io.close(file)
+end
